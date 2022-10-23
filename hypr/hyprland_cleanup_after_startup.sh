135b34b61cc813ef4c6e1a05bf0e0e6e9e68087f
daniel <daniel.pazos.e@gmail.com>
2022-10-23 06:10:49 +0500

    first commit

---
+++ hypr/hyprland_cleanup_after_startup.sh
@@ -0,0 +1,9 @@
+
+#! /bin/sh
+
+sleep 10
+
+hyprctl keyword windowrule "workspace unset,kitty"
+hyprctl keyword windowrule "workspace unset,spotify"
+hyprctl keyword windowrule "workspace unset,chronium"
+
